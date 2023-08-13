7af9e860bd4d1b71cce97255b4b6b0ff842a6e5e
Alex Kuzin <acidkarma3000@gmail.com>
2023-08-13 19:36:12 +0500

    Task 1 rework

--- src/test/java/ru/netology/DebitCardApplicationTest.java
+++ src/test/java/ru/netology/DebitCardApplicationTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,12 +13,12 @@ public class DebitCardApplicationTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-
-        SelenideElement form = $("[data-test-id]");
-        form.$("[data-test-id=name] input").setValue("Иванов Иван Иванович");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[button.button]").click();
-        $("data-test-id=order-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=name] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(byText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."))             // находит элемент по тексту целиком
+                .shouldBe(visible);
+        //$("data-test-id=order-success");//.shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
